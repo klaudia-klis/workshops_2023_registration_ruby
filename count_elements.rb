abcaa31627c0c178d08e4b7863e88cfbcadc141b
Klaudia <kklis@interia.pl>
2023-05-04 15:21:53 +0200

    Add solutions

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,13 @@
 class CountElements
-
+  def initialize(array)
+    @array = array
+  end
+  
+  def perform
+    result = Hash.new(0)
+    @array.each do |i|
+      result[i] += 1
+    end
+    return result
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,14 @@
 class ValidatePassword
-
+  def initialize(password)
+    @password = password
+  end
+  
+  def perform
+    validation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\!@#$%&*+=:;?<>])(?=.*[0-9])(?!.*(.)\1\1).{6,24}$/
+    if (@password.match(validation))
+      return true
+    else
+      return false
+    end
+  end
 end
